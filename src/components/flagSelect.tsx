de51f30e1b62338935ea1a52643cf6b59b92ff0e
LeoPedraza28 <leopedraza0828@gmail.com>
2025-08-09 16:21:17 -0400

    fix(flag-select): Fix i18n error

--- src/components/flagSelect.tsx
+++ src/components/flagSelect.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "react"
 import { useTranslation } from "react-i18next"
-import { i18next } from '../config/i18next'
+import { i18n } from '../config/i18next'
 
 import languages, { ILangauges } from '../config/languages'
 
@@ -14,7 +14,7 @@ export const FlagSelect = () => {
     const [selectedItem, setSelectedItem] = useState<ILangauges>()
 
     function changeLanguage(item: ILangauges) {
-        i18next.changeLanguage(item.value)
+        i18n.changeLanguage(item.value)
         setSelectedItem(item)
         setOpenDropdown(false)
         localStorage.setItem('lng', item.value)
@@ -37,24 +37,24 @@ export const FlagSelect = () => {
 
     return (
         <div className="relative w-max">
-            <button onClick={() => setOpenDropdown(!openDropdown)} type="button" className="relative p-2 text-center text-2xl font-semibold text-gray-600 rounded-full">
+            <button onClick={() => setOpenDropdown(!openDropdown)} type="button" className="relative p-2 text-2xl font-semibold text-center text-gray-600 rounded-full">
                 <img src={selectedItem?.flag} alt="" className="h-7 w-7" />
             </button>
 
             {
                 openDropdown &&
-                <div ref={modalRef} className="absolute z-10 mt-1 max-h-56 min-w-max right-0 overflow-auto rounded-md bg-white dark:bg-neutral-700 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
+                <div ref={modalRef} className="absolute right-0 z-10 py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 min-w-max dark:bg-neutral-700 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
                     {
                         languages.map((item) => (
-                            <div onClick={() => changeLanguage(item)} key={item.value} className="text-gray-900 relative cursor-pointer select-none py-2 pl-3 pr-9 gap-4">
+                            <div onClick={() => changeLanguage(item)} key={item.value} className="relative gap-4 py-2 pl-3 text-gray-900 cursor-pointer select-none pr-9">
                                 <div className="flex items-center">
-                                    <img src={item.flag} alt="" className="h-5 w-5 flex-shrink-0" />
-                                    <span className="font-normal ml-3 block truncate dark:text-white">{translations(`translate.${item.lang}`)}</span>
+                                    <img src={item.flag} alt="" className="flex-shrink-0 w-5 h-5" />
+                                    <span className="block ml-3 font-normal truncate dark:text-white">{translations(`translate.${item.lang}`)}</span>
                                 </div>
 
                                 {item.value == selectedItem?.value &&
-                                    <span className="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-2">
-                                        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 text-indigo-600">
+                                        <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
                                         </svg>
                                     </span>
